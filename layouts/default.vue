8ade46f865b121b7a2e8a874112bbfc1728209a4
Diyorbek Rajabov <diyorbek@abdigital.uz>
2024-09-13 22:45:18 +0500

    add Header layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<!-- layouts/default.vue -->
+<template>
+  <div>
+    <Header />
+    <main>
+      <NuxtPage />
+    </main>
+  </div>
+</template>
+
+<script setup>
+// Импортируем компонент заголовка
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
